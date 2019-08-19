04207b38d539955ccf087cb43d9890b3b28ee731
roman-melnik <roman-melnik91@mail.ru>
2019-08-19 21:16:54 +0300

    change product request from redux-thunk to redux-saga

--- src/actions/productsList.ts
+++ src/actions/productsList.ts
@@ -1,29 +1,15 @@
-import { Action, Dispatch, AnyAction } from 'redux';
-import { AxiosResponse, AxiosError } from 'axios';
+import { Action, AnyAction } from 'redux';
 
 import * as types from '../constants';
 import { IProductCard } from '../@types/productCard';
-import { axiosGetProductsList } from '../api';
 
 export interface IProductsAction extends Action {
   list: IProductCard[];
 }
 
-export interface IGetProductsList {(): (dispatch: Dispatch) => void; }
 export interface IAsyncGetProductsList { (): Action; }
 export interface IAsyncGetProductsListError { (error: any): AnyAction; }
 export interface IAsyncGetProductsListSuccess { (list: IProductCard[]): IProductsAction; }
-
-export const getProductsList: IGetProductsList  = () => {
-  return (dispatch) => {
-    dispatch(asyncGetProductsList());
-    axiosGetProductsList().then((response: AxiosResponse<IProductCard[]>) => {
-      dispatch(asyncGetProductsListSuccess(response.data));
-    }).catch((error: AxiosError) => {
-      dispatch(asyncGetProductsListError(error.message));
-    });
-  };
-};
 
 export const asyncGetProductsList: IAsyncGetProductsList = () => ({
   type: types.ASYNC_GET_PRODUCTS_LIST,
--- src/api/index.ts
+++ src/api/index.ts
@@ -4,5 +4,5 @@ import { IProductCard } from '../@types/productCard';
 const API_DATA_URL: string = 'http://localhost:3100/data';
 
 export const axiosGetProductsList = () => {
-  return axios.get<IProductCard[]>(API_DATA_URL);
+  return axios.get<IProductCard[]>(API_DATA_URL)
 };
